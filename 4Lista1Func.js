a124e87e7d433bf418b5e3f622f4917222299fb3
Eduardo Oliveira <eduardoykky@gmail.com>
2022-11-14 18:41:54 -0300

    quarto commit

--- 4Lista1Func.js
+++ 4Lista1Func.js
@@ -31,3 +31,41 @@ function ProcurarNome(nome) {
         }
     }   
 }
+
+var maiorId = 0
+
+function OrdernarIds() {
+    
+    suporteNome = []
+    supID = []
+    supPreco = []
+    supAva = []
+
+    for (var index = 0; index < ids.length; index++) {
+        maiorId = 0
+        for (var index1 = 0; index1 < ids.length; index1++) {
+            if (ids[index1] > maiorId) {
+                maiorId = ids[index1]
+            }
+        }
+        for (var index2 = 0; index2 < ids.length; index2++) {
+            if (maiorId == ids[index2]) {
+                supID[indexSup] = ids[index2]
+                suporteNome[indexSup] = nomes[index2]
+                supPreco[indexSup] = precos[index2]
+                supAva[indexSup] = avaliacoes[index2]
+                ids[index2] = 0
+                indexSup++
+            }
+        }
+    }
+    precos = supPreco
+    ids = supID
+    nomes = supID
+    avaliacoes = supAva
+    indexSup = 0
+
+    for (var index = 0; index < ids.length; index++) {
+        console.log(ids[index], nomes[index], precos[index], avaliacoes[index])
+    }
+}
